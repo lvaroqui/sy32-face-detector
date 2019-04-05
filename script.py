5a03dd76505021c4dc6b221e4be7482ee4072c71
Luc Varoqui <luc@varoqui.org>
2019-04-05 15:52:18 +0200

    Learned with HOG and SVC

--- script.py
+++ script.py
@@ -1,18 +1,13 @@
+import os
+from random import randint
+
+import numpy as np
+from skimage import feature
 from skimage import io
 from skimage import transform
 from skimage import util
-from skimage import color
-from skimage import feature
-from skimage import data
-from skimage import draw
-from scipy.ndimage import filters
-from random import randint
-from skimage import util
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
-import os
-
+from skimage.color import rgb2gray
+from matplotlib import pyplot as plt
 
 def load_from_folder(path):
     images = []
@@ -29,8 +24,13 @@ myLabels = np.loadtxt("label.txt", int)
 np.average(myLabels[:, 3] / myLabels[:, 4])
 
 
-# 2 - Cropping image from train datas
+# 2 - Cropping image from train data
 # On choisi une taille unique de 40*60
+
+I_INDEX = 1
+J_INDEX = 2
+HEIGHT_INDEX = 3
+WIDTH_INDEX = 4
 
 
 def resize_label(lbls):
@@ -38,15 +38,15 @@ def resize_label(lbls):
     # Resizing to ratio 1.5
     for face in labels:
         # If ratio smaller than 1.5
-        if face[3] / face[4] > 1.5:
-            previous = face[4]
-            face[4] = face[3] / 1.50
-            face[2] -= (face[4] - previous) / 2
+        if face[HEIGHT_INDEX] / face[WIDTH_INDEX] > 1.5:
+            previous = face[WIDTH_INDEX]
+            face[WIDTH_INDEX] = face[HEIGHT_INDEX] / 1.50
+            face[J_INDEX] -= (face[WIDTH_INDEX] - previous) / 2
         # If ratio smaller than 1.5
         else:
-            previous = face[3]
-            face[3] = face[4] * 1.50
-            face[1] -= (face[3] - previous) / 2
+            previous = face[HEIGHT_INDEX]
+            face[HEIGHT_INDEX] = face[WIDTH_INDEX] * 1.50
+            face[I_INDEX] -= (face[HEIGHT_INDEX] - previous) / 2
     return labels
 
 
@@ -79,7 +79,7 @@ def extract_faces(images, labels):
         image_labels = get_labels_from_image_index(i, labels)
         for label in image_labels:
             faces.append(crop_with_padding(images[i], label))
-    return np.array(faces)
+    return faces
 
 
 myLabels = np.loadtxt("label.txt", int)
@@ -92,27 +92,29 @@ faces = extract_faces(myImages, myLabelsResized)
 for i in range(len(faces)):
     faces[i] = transform.resize(faces[i], (60, 40), mode="constant")
 
+faces = np.array(faces)
+
 
 # 3 - Generating random no match pattern
 
-# Return the ration of intersection between two rectangle
+# Return the ratio of intersection between two rectangle
 # label format : [id, i, j, height, width]
 def intersec(rect1, rect2):
-    aire_rect1 = rect1[3] * rect1[4]
-    aire_rect2 = rect2[3] * rect2[4]
+    aire_rect1 = rect1[HEIGHT_INDEX] * rect1[WIDTH_INDEX]
+    aire_rect2 = rect2[HEIGHT_INDEX] * rect2[WIDTH_INDEX]
     intersec = 0
 
     # Check if the rectangles overlap
     if (not (
-            rect1[2] + rect1[4] < rect2[2] or rect1[2] > rect2[2] + rect2[4] or rect1[1] + rect1[3] < rect2[1] or
-            rect1[1] > rect2[1] + rect2[3])):
-        top = max(rect1[1], rect2[1])
-        bottom = min(rect1[1] + rect1[3], rect2[1] + rect2[3])
-        left = max(rect1[2], rect2[2])
-        right = min(rect1[2] + rect1[4], rect2[2] + rect2[4])
+            rect1[J_INDEX] + rect1[WIDTH_INDEX] < rect2[J_INDEX] or rect1[J_INDEX] > rect2[J_INDEX] + rect2[WIDTH_INDEX] or rect1[I_INDEX] + rect1[HEIGHT_INDEX] < rect2[I_INDEX] or
+            rect1[I_INDEX] > rect2[I_INDEX] + rect2[HEIGHT_INDEX])):
+        top = max(rect1[I_INDEX], rect2[I_INDEX])
+        bottom = min(rect1[I_INDEX] + rect1[HEIGHT_INDEX], rect2[I_INDEX] + rect2[HEIGHT_INDEX])
+        left = max(rect1[J_INDEX], rect2[J_INDEX])
+        right = min(rect1[J_INDEX] + rect1[WIDTH_INDEX], rect2[J_INDEX] + rect2[WIDTH_INDEX])
         intersec = (bottom - top) * (right - left)
 
-    return 2 * intersec / (aire_rect2 + aire_rect1)
+    return intersec / (aire_rect2 + aire_rect1 - intersec)
 
 
 # Generate random not face patches of size (40*60) from an image
@@ -121,34 +123,66 @@ def intersec(rect1, rect2):
 # labels : labels describing face on the image
 def generate_random_not_faces(number, image, labels):
     result_labels = []
-    while len(result_labels) != number:
+    while len(result_labels) < number:
         new_label = [0, 0, 0, 0, 0]
-        new_label[3] = randint(20, image.shape[0])
-        new_label[1] = randint(0, image.shape[0] - new_label[3])
-        new_label[4] = int(new_label[3] / 1.5)
-        new_label[2] = randint(0, image.shape[1] - new_label[4])
-
         is_good = True
+
+        new_label[HEIGHT_INDEX] = randint(20, image.shape[0])
+        new_label[I_INDEX] = randint(0, image.shape[0] - new_label[HEIGHT_INDEX])
+        new_label[WIDTH_INDEX] = int(new_label[HEIGHT_INDEX] / 1.5)
+        if new_label[WIDTH_INDEX] < image.shape[1]:
+            new_label[J_INDEX] = randint(0, image.shape[1] - new_label[WIDTH_INDEX])
+        else:
+            continue
+
         for label in labels:
-            if intersec(label, new_label) > 0.10:
+            if intersec(label, new_label) > 0.50:
                 is_good = False
-                break
+                continue
 
         if is_good:
             result_labels.append(new_label)
 
     result = []
     for label in result_labels:
-        zone = image[label[1]:label[1] + label[3], label[2]:label[2] + label[4]]
+        zone = image[label[I_INDEX]:label[I_INDEX] + label[HEIGHT_INDEX], label[J_INDEX]:label[J_INDEX] + label[WIDTH_INDEX]]
         result.append(transform.resize(zone, (60, 40), mode="constant"))
-
     return result
 
 
-rnds = generate_random_not_faces(10, myImages[0], get_labels_from_image_index(0, myLabels))
+not_faces = []
+for i in range(len(myImages)):
+    not_faces += generate_random_not_faces(10, myImages[i], get_labels_from_image_index(i, myLabels))
+not_faces = np.array(not_faces)
 
 
-# fig, ax = plt.subplots(1)
-# rect = Rectangle((rnd[2], rnd[1]), rnd[4], rnd[3])
-# ax.add_patch(rect)
-# plt.show()
+# Q4 - Apprendre un classifieur
+
+# HOG Computation
+hogs_not_faces = []
+for not_face in not_faces:
+    hogs_not_faces.append(feature.hog(rgb2gray(util.img_as_float(not_face))))
+hogs_not_faces = np.array(hogs_not_faces)
+
+hogs_faces = []
+for face in faces:
+    hogs_faces.append(feature.hog(rgb2gray(util.img_as_float(face))))
+hogs_faces = np.array(hogs_faces)
+
+hogs = np.concatenate((hogs_not_faces, hogs_faces), axis=0)
+targets = np.concatenate((np.full(hogs_not_faces.shape[0], -1.0), np.ones(hogs_faces.shape[0])))
+
+permutation = np.random.permutation(hogs.shape[0])
+
+hogs_shuffled = hogs[permutation]
+targets_shuffled = targets[permutation]
+
+# Learning
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import cross_validate
+clf = LinearSVC()
+clf.max_iter = 3000
+
+cv_results = cross_validate(clf, hogs_shuffled, targets_shuffled, cv=6, return_train_score=False)
+print(cv_results["test_score"])
+
