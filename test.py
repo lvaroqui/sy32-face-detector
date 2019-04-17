0f7e6c6dee0c2f7183a79b655de82585116e5f81
Luc Varoqui <luc@varoqui.org>
2019-04-17 23:53:20 +0200

    #1 test and send to professor

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from joblib import load
-from my_functions import load_from_folder, scan_images_multiprocessed
+from skimage import io
+from my_functions import load_from_folder, scan_images_multiprocessed, extract_rects, get_label_with_index
 
 if __name__ == '__main__':
     print("Loading classifier from \"finalClf.joblib\"")
@@ -10,6 +11,18 @@ if __name__ == '__main__':
     testImages = load_from_folder("test/")
 
     print("Finding faces on all images, this may take a while...")
-    myDetectedFaces = scan_images_multiprocessed(testImages, clf, 4)
+    myDetectedFaces = scan_images_multiprocessed(testImages, clf, 8, 5, 5, 2.5, 0.25)
+
+    j = 0
+    for i, image in enumerate(testImages):
+        rects = extract_rects(image, get_label_with_index(myDetectedFaces.astype(int), i))
+        for rect in rects:
+            print("----")
+            print(rect)
+            io.imsave("test_faces/" + str(j).zfill(4) + ".jpg", rect)
+            j += 1
+
+    myDetectedFaces[:, 0] = myDetectedFaces[:, 0] + 1
 
     np.savetxt("detections.txt", myDetectedFaces)
+
